20665683d01ce49ae1acf13a2a50e1b3ccbcfdb9
Yoonyesol <maniac970430@naver.com>
2024-01-11 21:17:54 +0900

    to do list Create - 새로운 투두 생성하기

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
@@ -27,11 +27,24 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    //content: input 태그에 사용자가 입력한 투두 항목
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content, //content로만 작성해도 가능(js 특성)
+      createdDate: new Date().getTime(),
+    }; //객체로 만드는 이유: 그렇게 설계해놨기 때문(mockData)
+
+    setTodos([...todos, newTodo]); //기존 배열+새로운 요소 추가
+  };
 
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   );
--- todolist/src/components/TodoEditor.jsx
+++ todolist/src/components/TodoEditor.jsx
@@ -1,10 +1,40 @@
+import { useRef, useState } from "react";
 import "./TodoEditor.css";
 
-export default function TodoEditor() {
+export default function TodoEditor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onClick = () => {
+    //빈 문자열일 때
+    if (content === "") {
+      inputRef.current.focus(); //input 창 focus만 진행
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onClick();
+    }
+  };
+
   return (
     <div className="TodoEditor">
-      <input placeholder="새로운 TODO..." />
-      <button>추가</button>
+      <input
+        ref={inputRef} //input태그에 inputRef가 접근할 수 있도록 해줌
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 TODO..."
+      />
+      <button onClick={onClick}>추가</button>
     </div>
   );
 }
