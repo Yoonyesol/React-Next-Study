5049e1b62ab532db95897ca6e934946bf4831e5f
Yoonyesol <maniac970430@naver.com>
2024-01-13 13:15:23 +0900

    React.memo: 컴포넌트 리렌더 방지하기

--- todolist/src/components/Header.jsx
+++ todolist/src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { memo } from "react";
 import "./Header.css";
 
-export default function Header() {
+function Header() {
   return (
     <div className="Header">
       <h1>{new Date().toDateString()}</h1>
     </div>
   );
 }
+
+//memo: 인수로 컴포넌트 전달받아서 최적화된 컴포넌트로 반환
+const OptimizedHeaderComponent = memo(Header);
+
+export default OptimizedHeaderComponent;
