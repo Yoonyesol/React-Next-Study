a7eeaf89259a9073e0cc86cc230a0f97aa1b04f2
Yoonyesol <maniac970430@naver.com>
2024-01-14 16:22:05 +0900

    [PRJ2] Naras: 페이지 구현 2. 컨트리

--- naras/src/pages/Country.jsx
+++ naras/src/pages/Country.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchCountry } from "../api";
+import style from "./Country.module.css";
 
 export default function Country() {
   const params = useParams(); //URL 파라미터 값 꺼내오기. params 안에 현재 url의 값이 객체 형태로 담김
@@ -15,5 +16,40 @@ export default function Country() {
     setInitData();
   }, [params.code]);
 
-  return <div>Country: {params.code}</div>;
+  if (!country) {
+    //아직 api 호출 응답이 전달되기 전 country=undefined 오류 해결
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.commonName}>
+          {country.flagEmoji}&nbsp;{country.commonName}
+        </div>
+        <div className={style.officialName}>{country.officialName}</div>
+      </div>
+      <img
+        src={country.flagImg}
+        alt={`${country.commonName}의 국가 이미지입니다.`}
+      />
+      <div className={style.body}>
+        <div>
+          <b>코드: </b>&nbsp;{country.code}
+        </div>
+        <div>
+          <b>수도: </b>&nbsp;{country.capital.join(", ")}
+        </div>
+        <div>
+          <b>지역: </b>&nbsp;{country.region}
+        </div>
+        <div>
+          <b>지도: </b>&nbsp;
+          <a target="_blank" href={country.googleMapURL}>
+            {country.googleMapURL}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
