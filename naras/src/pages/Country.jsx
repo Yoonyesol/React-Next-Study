72e935522453487a23b29eaf4d62b884efbd3d43
Yoonyesol <maniac970430@naver.com>
2024-01-14 11:29:11 +0900

    [PRJ2] Naras: API 호출하기 2

--- naras/src/pages/Country.jsx
+++ naras/src/pages/Country.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchCountry } from "../api";
 
 export default function Country() {
   const params = useParams(); //URL 파라미터 값 꺼내오기. params 안에 현재 url의 값이 객체 형태로 담김
+  const [country, setCountry] = useState();
+
+  const setInitData = async () => {
+    const data = await fetchCountry(params.code);
+    setCountry(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, [params.code]);
 
   return <div>Country: {params.code}</div>;
 }
--- naras/src/pages/Search.jsx
+++ naras/src/pages/Search.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { fetchSearchResults } from "../api";
 
 export default function Search() {
   //searchParams: 현재 어떤 쿼리 스트링이 저장되어 있는지 정보를 담고 있는 객체
   //setSearchParams: 현재 쿼리 스트링을 변경할 수 있는 함수
   const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
 
-  //http://localhost:5173/search?q=123 이렇게 입력하면 화면에 123이 뜬다.
-  return <div>{searchParams.get("q")}</div>;
+  const [countries, setCountries] = useState([]);
+
+  const setInitData = async () => {
+    const data = await fetchSearchResults(q);
+    setCountries(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, [q]);
+
+  return <div></div>;
 }
