b85b9defaf88be07837ed836183c38527c1b73e9
Yoonyesol <maniac970430@naver.com>
2024-01-14 11:10:23 +0900

    [PRJ2] Naras: API 호출하기 1

--- naras/src/pages/Home.jsx
+++ naras/src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
+
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+
+  const setInitData = async () => {
+    //비동기 코드이므로 불러올 때도 await 필요
+    const data = await fetchCountries();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, []);
+
   return <div></div>;
 }
