819ea39bc98d7e1158ecd1754aba34890d2b66c0
Yoonyesol <maniac970430@naver.com>
2024-01-21 22:42:50 +0900

    Next.js 페이지 라우터: 페이지별 레이아웃 설정하기

--- section12/pages/_app.js
+++ section12/pages/_app.js
@@ -2,9 +2,18 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  //console.log(Component.Layout);
+  //Component가 Search 컴포넌트라면 SubLayout 값이 호출된다.
+
+  const EmptyLayout = ({ children }) => <>{children}</>;
+  //컴포넌트에 붙어있는 레이아웃이 있다면 그걸 사용하고 아니라면 그냥 children을 감싸주기만 하는 빈 레이아웃 적용
+  const SubLayout = Component.Layout || EmptyLayout;
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <SubLayout>
+        <Component {...pageProps} />
+      </SubLayout>
     </Layout>
   );
 }
--- section12/pages/search/index.js
+++ section12/pages/search/index.js
@@ -1,3 +1,9 @@
+import SubLayout from "@/components/SubLayout";
+
 export default function Search() {
   return <div>Search Page</div>;
 }
+
+Search.Layout = SubLayout;
+//Search: 객체(자바스크립트의 모든 함수는 객체이므로)
+//객체에 Layout이라는 프로퍼티를 추가하고 그 값으로 SubLayout 객체를 저장
