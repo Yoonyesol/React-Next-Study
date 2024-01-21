cd0bdbc8e8f7d5e465b80e9f3911ec86afbf7344
Yoonyesol <maniac970430@naver.com>
2024-01-21 21:38:23 +0900

    Next.js 페이지 라우터 기본 및 페이지 이동하기

---
+++ section12/pages/country/[code].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+export default function Country() {
+  const router = useRouter();
+  // const code = router.query.code; //URL파라미터로 전달된 국가코드 꺼내오기
+  const { code } = router.query; //구조분해 할당
+
+  return <div>Country {code}</div>;
+}
