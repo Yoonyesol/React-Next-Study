5bb7a1aff8115a5ea6547c2982bb5a8290a2d875
Yoonyesol <maniac970430@naver.com>
2024-01-22 23:04:14 +0900

    SSG(정적 사이트 생성) 적용하기 3, 4

--- section12/pages/country/[code].js
+++ section12/pages/country/[code].js
@@ -7,6 +7,16 @@ export default function Country({ country }) {
   // const code = router.query.code; //URL파라미터로 전달된 국가코드 꺼내오기
   const { code } = router.query; //구조분해 할당
 
+  // props를 기다리는 상태
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  // 혹시 api를 통해 데이터를 불러오는 것이 실패했을 때를 대비한 예외처리
+  if (!country) {
+    return <div>존재하지 않는 국가입니다.</div>;
+  }
+
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -19,7 +29,7 @@ Country.Layout = SubLayout; //SubLayout을 적용시켜준다.
 export const getStaticPaths = async () => {
   return {
     path: [{ params: { code: "ABW" } }, { params: { code: "KOR" } }],
-    fallback: false, //path에 명시하지 않은 경로의 요청이 오면 404페이지 반환
+    fallback: true,
   };
 };
 
--- section12/pages/search/index.js
+++ section12/pages/search/index.js
@@ -1,7 +1,27 @@
 import { fetchSearchResults } from "@/api";
 import SubLayout from "@/components/SubLayout";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export default function Search({ countries }) {
+export default function Search() {
+  const router = useRouter();
+  const { q } = router.query; //쿼리 스트링 추출
+
+  const [countries, setCountries] = useState([]); //국가 리스트 저장
+
+  const setData = async () => {
+    const data = await fetchSearchResults(q);
+    setCountries(data);
+  };
+
+  // 컴포넌트 마운트 or 쿼리스트링 변화시에 실행
+  useEffect(() => {
+    if (q) {
+      //쿼리스트링 존재시에만 리스트를 불러오기
+      setData();
+    }
+  }, [q]);
+
   return (
     <div>
       {countries.map((country) => (
@@ -12,22 +32,3 @@ export default function Search({ countries }) {
 }
 
 Search.Layout = SubLayout;
-
-//쿼리를 이용한 동적 라우팅을 위해 context 이용
-//context: 브라우저의 접속요청에 대한 정보들이 저장되어 있다.
-export const getServerSideProps = async (context) => {
-  // 1. 검색 결과 api 호출
-  // 2. props 리턴
-
-  // const q = context.query.q;
-  const { q } = context.query;
-
-  let countries = [];
-  if (q) {
-    countries = await fetchSearchResults(q);
-  }
-
-  return {
-    props: { countries },
-  };
-};
