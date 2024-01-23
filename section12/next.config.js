cd68db66b213da5469a978e620d0e10e49b2a7fa
Yoonyesol <maniac970430@naver.com>
2024-01-23 21:41:32 +0900

    [PRJ3] Next.js Naras: 이미지 최적화하기

--- section12/next.config.js
+++ section12/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    //외부이미지주소 사용 등록
+    domains: ["flagcdn.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
